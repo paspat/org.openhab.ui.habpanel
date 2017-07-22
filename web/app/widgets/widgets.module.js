a418b37c5d6ac89e5612c86bc0f47ffd0facd02e
Patrick Spiss <paspat2016@gmail.com>
2017-07-22 16:32:42 +0200

    Add a fit-text directive to use the available space (e.g in a div) and set the font-size accordingly. This is useful to auto adjust the text size to fit to the widget size. There are several options for the directive:
    minSize: the minimum size of the font (default is 1)
    grow: set to false if the font size should not grow (default is true)
    shrink: set to false if the font size should not shrink (default is
    true)
    compressor: a factor to increase / decrease the calculated size (default
    is 1); us a value < 0 to make the font smaller
    
    Signed-off-by: Patrick Spiss <paspat2016@gmail.com>

--- web/app/widgets/widgets.module.js
+++ web/app/widgets/widgets.module.js
@@ -18,6 +18,7 @@
     .directive('widgetIcon', WidgetIcon)
     .directive('itemTypeIcon', ItemTypeIcon)
     .directive('itemPicker', ItemPicker)
+    .directive('fitText',AutoFitText)
     .filter('themeValue', ThemeValueFilter)
     
 
@@ -200,4 +201,140 @@
             return (themeValue) ? themeValue.trim() : null;
         }
     }
+    
+    AutoFitText.$inject = ['$window'];
+    function AutoFitText($window) {
+        return {
+            template: '<div data-role="inner" ng-transclude></div>',
+            transclude: true,
+            link: {
+                post: function(scope, elem, attrs) {
+                    var providedOptions = scope.$eval(attrs.fitText) || {};
+                    var options = angular.extend({
+                        shrink: true,
+                        grow: true,
+                        minSize: 1,
+                        compressor: 1
+                    }, providedOptions);
+
+                    var inner = angular.element(elem[0].querySelector('div[data-role]'));
+                    
+                    scope.$watch( function() {
+                        return elem[0].offsetHeight;
+                    }, function() {
+                        resize();
+                    });
+                    
+                    scope.$watch( function() {
+                        return elem[0].offsetWidth;
+                    }, function() {
+                        resize();
+                    });                    
+
+                    function resize(timeout){
+                        timeout = (timeout==null) ? 0 : 2000;
+                        scope.timeout = setTimeout(function(){
+                            if(elem[0].clientWidth>0){
+                                shrinkOrGrow();
+                            }
+                        },timeout);                        
+                    }
+                    
+                    function getSize(element){
+                        var cs = getComputedStyle(element);
+                        
+                        var paddingX = parseFloat(cs.paddingLeft || 0) + parseFloat(cs.paddingRight || 0);
+                        var paddingY = parseFloat(cs.paddingTop || 0) + parseFloat(cs.paddingButtom || 0);
+
+                        var borderX = parseFloat(cs.borderLeftWidth || 0) + parseFloat(cs.borderRightWidth || 0);
+                        var borderY = parseFloat(cs.borderTopWidth || 0) + parseFloat(cs.borderBottomWidth || 0);
+
+                        // Element width and height minus padding and border
+                        var elementWidth = element.offsetWidth - paddingX - borderX;
+                        var elementHeight = element.offsetHeight - paddingY - borderY; 
+
+                        return {width: elementWidth, height: elementHeight};
+                    }
+
+                    function shrinkOrGrow() {
+                        var i = 0;
+
+                        // deal with line-height and images
+                        adjustLineHeightAndInlineImages();
+
+                        if (fontTooBig() && options.shrink) {
+                            while (fontTooBig() && i < 100 && fontSizeI() >= options.minSize) {
+                                setFontSize(fontSizeI() - 1);
+                                i = i + 1;
+                            }                           
+                            
+                        } else if (fontTooSmall() && options.grow) {
+                            while (fontTooSmall() && i < 100) {
+                                setFontSize(fontSizeI() + 1);
+                                i = i + 1;                               
+                                
+                                if(fontTooBig())
+                                    setFontSize(fontSizeI() - 1);                                
+                            }                            
+                        } else {
+                            return;
+                        }
+
+                        if(options.compressor!=1) 
+                            setFontSize(fontSizeI() * options.compressor);                        
+
+                        scope.$emit('fit-text:resized', {
+                            fontSize: fontSizeI(),
+                            elem: elem
+                        });
+                    }
+
+                    function css(el, prop) {
+                        if ($window.getComputedStyle) return $window.getComputedStyle(el[0]).getPropertyValue(prop);
+                    }
+
+                    function fontSizeI() {
+                        var fontSize = css(inner, 'font-size');
+                        var match = fontSize.match(/\d+/);
+                        if (!match) {
+                            return;
+                        }
+                        return Number(match[0]);
+                    }
+
+                    function setFontSize(size) {
+                        inner[0].style.fontSize = size + 'px';
+                        adjustLineHeightAndInlineImages();
+                    }
+
+                    function adjustLineHeightAndInlineImages() {
+                        if (!fontSizeAdjusted()) {
+                            return;
+                        }
+                        var size = fontSizeI();
+                        inner[0].style.lineHeight = (size + 2) + 'px';
+                        var images = inner[0].querySelectorAll('img');
+                        angular.forEach(images, function(img) {
+                            img.style.height((size + 2) + 'px');
+                        });
+                    }
+
+                    function fontSizeAdjusted() {
+                        return !!inner[0].style.fontSize;
+                    }
+
+                    function fontTooBig() {
+                        var elemSize = getSize(elem[0]);
+                        return (inner[0].offsetWidth > elemSize.width || inner[0].offsetHeight > elemSize.height);
+                    }
+
+                    function fontTooSmall() {
+                        var elemSize = getSize(elem[0]);                      
+                        return (inner[0].offsetWidth < elemSize.width || inner[0].offsetHeight < elemSize.height);
+                    }
+
+                }
+            }
+        };
+    }    
 })();
